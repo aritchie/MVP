a2f46bb9d760368f350b22bed76f8d9f788ec32f
Allan Ritchie <allan.ritchie@gmail.com>
2020-02-01 13:59:08 -0500

    add shiny requirements

---
+++ MVP/Services/DataSyncJob.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Shiny.Jobs;
+
+
+namespace MVP.Services
+{
+    public class DataSyncJob : IJob
+    {
+        public async Task<bool> Run(JobInfo jobInfo, CancellationToken cancelToken)
+        {
+            return true;
+        }
+    }
+}
---
+++ MVP/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using MVP.Services;
+using Shiny;
+using Shiny.Jobs;
+
+
+namespace MVP
+{
+    public class Startup : ShinyStartup
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            services.UseNotifications();
+            services.RegisterJob(new JobInfo(typeof(DataSyncJob))
+            {
+                BatteryNotLow = true,
+                RequiredInternetAccess = InternetAccess.Any
+            });
+        }
+    }
+}
