c9b26089ae0df9669cb186ccf5eb19485e303bec
Allan Ritchie <allan.ritchie@gmail.com>
2020-02-02 13:13:05 -0500

    sqlite

---
+++ MVP/Services/MvpSqliteConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using SQLite;
+
+
+namespace MVP.Services
+{
+    public class MvpSqliteConnection : SQLiteConnection
+    {
+        public MvpSqliteConnection() : base(Path.Combine(Xamarin.Essentials.FileSystem.AppDataDirectory, "mvp.db"))
+        {
+        }
+    }
+}
